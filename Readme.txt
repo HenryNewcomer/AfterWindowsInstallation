6566431df00d73b9856355f29a2ab4521c19dffb
Henry Newcomer <a.cliche.email@gmail.com>
2019-01-27 23:12:00 -0600

    Started working on Windows setup; sick of redoing EVERYTHING

---
+++ Readme.txt
@@ -0,0 +1,10 @@
+﻿*** Make sure that you're running PowerShell with Administrative rights ***
+
+Run:
+Get-ExecutionPolicy
+
+If it returns "Restricted" run:
+Set-ExecutionPolicy AllSigned
+
+Alternatively, you can run:
+Set-ExecutionPolicy Bypass -Scope Process
---
+++ init.ps1
@@ -0,0 +1,6 @@
+﻿# Linux subsystem
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+# Manually install Ubuntu from: https://www.microsoft.com/en-us/p/ubuntu/9nblggh4msv6?rtc=1&activetab=pivot:overviewtab
+
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
