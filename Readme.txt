9e545f47e014f271775fb913391f6cc39df6cb05
Henry Newcomer <a.cliche.email@gmail.com>
2019-06-01 17:20:14 -0500

    Remade the auto-setup; uses Chocolatey for package installs

--- Readme.txt
+++ Readme.txt
@@ -1,10 +1,25 @@
-﻿*** Make sure that you're running PowerShell with Administrative rights ***
+﻿
+/////////////////////
+Pre-config setup:
+/////////////////////
 
-Run:
-Get-ExecutionPolicy
+// PART ONE
 
-If it returns "Restricted" run:
-Set-ExecutionPolicy AllSigned
+1) Open PowerShell **as Administrator**
 
-Alternatively, you can run:
+2) In order to run the subsystem, enter:
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+
+3) Restart the computer
+
+// PART TWO
+
+4) Open PowerShell **as Administrator**
+
+5) Move to this project's root directory
+
+6) Run the following two commands:
 Set-ExecutionPolicy Bypass -Scope Process
+.\init.ps1
+
+# Note: You can check the current policy via: Get-ExecutionPolicy
--- init.ps1
+++ init.ps1
@@ -1,6 +1,45 @@
-﻿# Linux subsystem
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-# Manually install Ubuntu from: https://www.microsoft.com/en-us/p/ubuntu/9nblggh4msv6?rtc=1&activetab=pivot:overviewtab
+﻿#Requires -RunAsAdministrator
 
-# Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# A list of packages to install via Chocolatey
+$chocolateyPackages = @("llvm")
+
+# TODO Stop the recursive nature of this
+function henry-showSetupSteps {
+    $setupSteps = [Management.Automation.Host.ChoiceDescription[]] @(
+        New-Object Management.Automation.Host.ChoiceDescription("&Ubuntu","Ubuntu download")
+        New-Object Management.Automation.Host.ChoiceDescription("&Chocolatey (Install)","Basic Chocolatey installation.")
+        New-Object Management.Automation.Host.ChoiceDescription("Chocolatey (&Packages)","Install Chocolatey packages.")
+        New-Object Management.Automation.Host.ChoiceDescription("&Quit","Abort this script.")
+    )
+    $global:selection = $Host.UI.PromptForChoice("Auto-Setup Step Selection","Which step would you like to proceed with?",$setupSteps,3)
+
+    if ($selection -eq 0) {
+        echo "Attempting to download Ubuntu 18.04"
+        Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile Ubuntu.appx -UseBasicParsing
+        .\Ubuntu.appx
+        henry-showSetupSteps
+    } elseif ($selection -eq 1) {
+        echo "Attempting to install Chocolatey"
+        Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+        henry-showSetupSteps
+    } elseif ($selection -eq 2) {
+        echo "Attempting to install Chocolatey packages"
+        # TODO Save a txt/cache of packages that were (or weren't?) installed
+        foreach($package in $chocolateyPackages) {
+            choco install $package -y
+            if (Get-Package $package | Install-Package) {
+                continue;
+            } else {
+                echo "$package wasn't installed!"
+                pause
+            }
+        }
+        henry-showSetupSteps
+    } # else quiting...
+}
+
+# -----------------
+
+#echo "Automated setup for new Windows installation will start in a few seconds..."
+#Start-Sleep 4
+henry-showSetupSteps
